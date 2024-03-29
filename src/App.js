f871462c47f30193be1f20e7f1eaafdd7fb78a81
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-03-29 17:39:08 +0100

    DotEnv added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const neowsapi = () => {
+  const [ast, setAst] = useState([])
+
+  useEffect(() => {
+    const getAst = async () => {
+      const response = await axios.get(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${process.env.API_KEY}`)
+    }
+  })
 }
 
 export default App;
