daa6b5ca69ef7725cd9e1036deaccc17e7200b68
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-03-29 17:41:29 +0100

    Return call added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,32 @@ const neowsapi = () => {
     getAst()
   }, [])
 
-  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Asteroids List</h1>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Distance from Earth (km)</th>
+            <th>Orbiting</th>
+          </tr>
+        </thead>
+        <tbody>
+          {asteroids.map((asteroid, index) => (
+            <tr key={index}>
+              <td>{asteroid.name}</td>
+              <td>{asteroid.close_approach_data[0].miss_distance.km}</td>
+              <td>{asteroid.orbiting_body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 
 }
 
